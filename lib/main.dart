202e1c5976904c59d90b0845c08ee39fab9d8d78
cimplesid <joshisid178@gmail.com>
2020-08-28 19:44:16 +0545

    google font added

--- lib/main.dart
+++ lib/main.dart
@@ -3,46 +3,23 @@ import 'package:dism/helpers/firebase.dart';
 import 'package:dism/screens/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:snowm_scanner/snowm_scanner.dart';
 
 import 'screens/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
-}
+  snowmScanner.configure(enableMqtt: true);
+  var user = await firebase.init();
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-    init();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'DSM',
-      theme: ThemeData(primaryColor: primaryColor),
-      home: isLoggedIn ? Home() : LoginScreen(),
-    );
-  }
-
-  bool isLoggedIn = false;
-  init() async {
-    var user = await firebase.init();
-    if (user != null) {
-      setState(() {
-        isLoggedIn = true;
-      });
-    } else
-      setState(() {
-        isLoggedIn = false;
-      });
-  }
+  runApp(GetMaterialApp(
+    home: user != null ? Home() : LoginScreen(),
+    theme: ThemeData(
+        textTheme: GoogleFonts.montagaTextTheme(), primaryColor: primaryColor),
+    debugShowCheckedModeBanner: false,
+    title: 'Cave cafe',
+  ));
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:snowm_scanner/snowm_scanner.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,6 +10,53 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<SnowMBeacon> detectedBeacons;
+  ScaffoldFeatureController<SnackBar, SnackBarClosedReason> snackbar;
+  BluetoothState state;
+  List<StreamSubscription> subs = [];
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero, () {
+      startListening();
+    });
+  }
+
+  startListening() async {
+    var ids = [""];
+    snowmScanner.getBluetoothStateStream().listen((state) async {
+      if (this.mounted)
+        setState(() {
+          this.state = state;
+        });
+      if (state == BluetoothState.ON) {
+        if (snackbar != null) snackbar.close();
+        subs.add(snowmScanner
+            .scanBeacons(uuids: ids, scanAllBeacons: true)
+            .listen(setBeacons));
+      } else if (state == BluetoothState.OFF) {
+        showSnackbar("Please turn on bluetooth.");
+      }
+    });
+  }
+
+  showSnackbar(String message) {
+    Get.snackbar(
+      'Turn on Bluetooth',
+      message,
+      snackPosition: SnackPosition.BOTTOM,
+      duration: Duration(seconds: 60),
+    );
+  }
+
+  setBeacons(List<SnowMBeacon> allDetectedBeacons) async {
+    print(allDetectedBeacons);
+    if (mounted)
+      setState(() {
+        detectedBeacons = allDetectedBeacons;
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,9 +64,7 @@ class _HomeState extends State<Home> {
         title: Text('Home'),
       ),
       body: Column(
-        children: <Widget>[
-            
-        ],
+        children: <Widget>[],
       ),
     );
   }
